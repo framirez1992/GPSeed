1166eb109f4054cab129c9da38ee26215e633425
Francisco Ramirez <framirez777@outlook.com>
2019-05-29 06:40:22 -0400

    agegado intefaz

---
+++ app/src/main/java/com/far/gpseed/Interfaces/ListableActivity.java
@@ -0,0 +1,11 @@
+package com.far.gpseed.Interfaces;
+
+import com.far.gpseed.Models.SeedLocation;
+
+/**
+ * Created by Framirez on 26/05/2019.
+ */
+
+public interface ListableActivity {
+    void onClick(SeedLocation seed);
+}
